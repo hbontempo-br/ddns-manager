bd1f89eba47610dc4a49d097e3f136a7ce0a98e8
Henrique Santaella Bontempo <henrique.s.bontempo@gmail.com>
2019-06-10 01:02:35 -0300

    First code commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import time
+import logging
+
+from ipify import get_ip
+from requests import get, Response
+from retry import retry
+
+from constants import *
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def validate_google_response(response: Response):
+    response.raise_for_status()
+    if not ("good" in response.text or "nochg" in response.text):
+        raise Exception(f"Bad response: {response.text}")
+
+
+@retry(tries=3, delay=2)
+def update_routine(username: str, password: str, hostname: str, ip: str):
+    new_ip = get_ip()
+    logging.debug(f"External IP: {new_ip}")
+    if new_ip != ip:
+        url = f"https://{username}:{password}@domains.google.com/nic/update?hostname={hostname}&myip={new_ip}"
+        response = get(url)
+        validate_google_response(response=response)
+        logging.debug(f"Record updated: {ip} > {new_ip}")
+        return new_ip
+    return ip
+
+
+ip = ""
+logging.debug("Starting application")
+while True:
+    ip = update_routine(username=USERNAME, password=PASSWORD, hostname=HOSTNAME, ip=ip)
+    logging.debug(f"Sleeping: {UPDATE_DELAY}")
+    time.sleep(UPDATE_DELAY)
---
+++ constants.py
@@ -0,0 +1,7 @@
+import os
+
+USERNAME = os.environ.get("USERNAME") or "A2H0WRnyN94YPq4A"
+PASSWORD = os.environ.get("PASSWORD") or "jrgcxioY1uvYzmdg"
+HOSTNAME = os.environ.get("HOSTNAME") or "house.hbontempo.dev"
+
+UPDATE_DELAY = int(os.environ.get("UPDATE_DELAY", 10))
