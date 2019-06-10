bdf72d43ebc45a3b59b87b4ee2f4096bbb02f525
Henrique Santaella Bontempo <henrique.s.bontempo@gmail.com>
2019-06-10 02:56:48 -0300

    Ups..

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import os
 
-USERNAME = os.environ.get("USERNAME") or "A2H0WRnyN94YPq4A"
-PASSWORD = os.environ.get("PASSWORD") or "jrgcxioY1uvYzmdg"
-HOSTNAME = os.environ.get("HOSTNAME") or "house.hbontempo.dev"
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
+HOSTNAME = os.environ.get("HOSTNAME")
 
 UPDATE_DELAY = int(os.environ.get("UPDATE_DELAY", 10))
