fbef497639a11d614ebbbe8fc74522c6c23ccec8
Daniel Augusto <danieldeveloper001@gmail.com>
2019-06-17 20:52:31 -0300

    :sparkles: Adds console and file logger configuration functions.

--- app.py
+++ app.py
@@ -7,10 +7,18 @@ from retry import retry
 
 from constants import *
 
+from src.configuration.logger import setup_console_logging
 from src.helper.environment import get_environment_variable
 
-logging.basicConfig(level=logging.DEBUG)
 
+# CONFIGURES LOGGER
+logger_format = "%(asctime)s | %(levelname)s - %(message)s"
+logger_date_time_format = "%Y-%m-%d %H:%M:%S"
+logger = setup_console_logging(
+    logging.DEBUG,
+    logger_format,
+    logger_date_time_format
+)
 
 def validate_google_response(response: Response):
     response.raise_for_status()
@@ -35,5 +43,5 @@ ip = ""
 logging.debug("Starting application")
 while True:
     ip = update_routine(username=USERNAME, password=PASSWORD, hostname=HOSTNAME, ip=ip)
-    logging.debug(f"Sleeping: {UPDATE_DELAY}")
+    logger.debug(f"Sleeping: {UPDATE_DELAY}")
     time.sleep(UPDATE_DELAY)
