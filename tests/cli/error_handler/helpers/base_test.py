ed2fb8e86488b96e04d83717844c4522812b2baa
Henrique Bontempo <me@hbontempo.dev>
2021-06-30 18:10:24 -0300

    Adds test to cli/error_handler/helpers/base

---
+++ tests/cli/error_handler/helpers/base_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from tests.common import *
+from ddns_manager.cli.error_handler.helpers.base import ErrorHandlerConfigHelper
+
+
+class TestErrorHandlerConfigHelper(unittest.TestCase):
+    def test_build_signature(self):
+        # test if ConfigHelper has the method build and if that has the correct signature
+
+        # has should have method
+        method = getattr(ErrorHandlerConfigHelper, 'build', None)
+        self.assertTrue(callable(method))
+
+        # check if is abstract
+        self.assertTrue(is_abstract(method))
+
+        # checks method signature
+        self.assertEqual('(config: Dict) -> Callable[[Exception], NoneType]', callable_signature(method))
