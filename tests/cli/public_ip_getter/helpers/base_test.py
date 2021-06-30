4ce337690bff54207d8d742095a7a1ec98710fd4
Henrique Bontempo <me@hbontempo.dev>
2021-06-30 18:04:27 -0300

    Adds test to cli/public_ip_getter/helpers/base

---
+++ tests/cli/public_ip_getter/helpers/base_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from tests.common import *
+from ddns_manager.cli.public_ip_getter.helpers.base import PublicIpGetterConfigHelper
+
+
+class TestPublicIpGetterConfigHelper(unittest.TestCase):
+    def test_build_signature(self):
+        # test if ConfigHelper has the method build and if that has the correct signature
+
+        # has should have method
+        method = getattr(PublicIpGetterConfigHelper, 'build', None)
+        self.assertTrue(callable(method))
+
+        # check if is abstract
+        self.assertTrue(is_abstract(method))
+
+        # checks method signature
+        self.assertEqual('(config: Dict) -> ddns_manager.public_ip_getter.base.PublicIPGetter', callable_signature(method))
