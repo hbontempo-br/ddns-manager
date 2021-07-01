a330f51985e50b77f3b71ddabd43299bfde94951
Henrique Bontempo <me@hbontempo.dev>
2021-07-01 11:47:15 -0300

    Adds test to cli/public_ip_getter/base

---
+++ tests/cli/public_ip_getter/base_test.py
@@ -0,0 +1,43 @@
+import unittest
+
+from ddns_manager import GooglePublicIpGetter
+from ddns_manager.cli.public_ip_getter.base import factory, public_ip_getter
+from ddns_manager.cli.public_ip_getter.helpers import GoogleConfigHelper
+
+
+class TestPublicIpGetterCLI(unittest.TestCase):
+    def test_factory_with_valid_type(self):
+        cases = [
+            ('google', GoogleConfigHelper)
+        ]
+        for ddns_type, expected_response in cases:
+            self.assertIs(expected_response, factory(ddns_type))
+
+    def test_factory_with_invalid_type(self):
+        self.assertRaises(AttributeError, factory, 'invalid type')
+
+    def test_public_ip_getter_with_valid_config(self):
+        cases = [
+            (
+                {
+                    "type": 'google',
+                    "details": None
+                },
+                GooglePublicIpGetter
+            ),
+            (
+                {
+                    "type": 'google',
+                    "details": {}
+                },
+                GooglePublicIpGetter
+            ),
+            (
+                {
+                    "type": 'google'
+                },
+                GooglePublicIpGetter
+            )
+        ]
+        for config, expected_response in cases:
+            self.assertIsInstance(public_ip_getter(config), expected_response)
