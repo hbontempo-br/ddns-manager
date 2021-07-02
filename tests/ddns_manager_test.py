d15b2883f9aa78b1a8564216ac655b5c22683bcf
Henrique Bontempo <me@hbontempo.dev>
2021-07-02 10:39:42 -0300

    Adds test to DDNSManager loop

--- tests/ddns_manager_test.py
+++ tests/ddns_manager_test.py
@@ -1,5 +1,7 @@
 import unittest
+from datetime import datetime, timedelta
 from typing import Callable, NoReturn
+
 from ddns_manager import DDNSManager
 from ddns_manager.ddns_updater import DDNSUpdater
 from ddns_manager.public_ip_getter import PublicIPGetter
@@ -39,18 +41,36 @@ class MockDDNSUpdater(DDNSUpdater):
 
 
 class CallRecorder:
-    def __init__(self, return_value):
-        self.ret = return_value
-        self.args = None
-        self.kwargs = None
-        self.count = 0
+    def __init__(self, on_call):
+        self.on_call = on_call
+        self.calls = []
 
     def __call__(self, *args, **kwargs):
-        self.count = self.count + 1
-        self.args = args
-        self.kwargs = kwargs
+        try:
+            response = self.on_call()
+            self.calls.append(
+                {
+                    'args': args,
+                    'kwargs': kwargs,
+                    'timestamp': datetime.now(),
+                    'response': response
+                }
+            )
+        except Exception as e:
+            self.calls.append(
+                {
+                    'args': args,
+                    'kwargs': kwargs,
+                    'timestamp': datetime.now(),
+                    'exception': e
+                }
+            )
+            raise e
+        return response
 
-        return self.ret
+    @property
+    def count(self):
+        return len(self.calls)
 
 
 class TestDDNSManager(unittest.TestCase):
@@ -73,7 +93,7 @@ class TestDDNSManager(unittest.TestCase):
     def test_current_ip_should_call_public_ip_getter(self):
         # current_ip should call PublicIPGetter#get_current_ip()
         whatever = 'whatever'
-        mc = CallRecorder(whatever)
+        mc = CallRecorder(on_call=lambda: whatever)
         mock_pig = MockPublicIPGetter(get_current_ip_action=mc)
         mock_du = MockDDNSUpdater(update_ddns_record_action=lambda: None)
 
@@ -124,7 +144,7 @@ class TestDDNSManager(unittest.TestCase):
         )
 
         test_ip2 = 'test_ip2'
-        mc = CallRecorder(test_ip2)
+        mc = CallRecorder(on_call=lambda: test_ip2)
         mock_pig.get_current_ip_action = mc
         self.assertEqual(0, mc.count)
 
@@ -176,7 +196,7 @@ class TestDDNSManager(unittest.TestCase):
         )
 
         test_ip2 = 'test_ip2'
-        mc = CallRecorder(test_ip2)
+        mc = CallRecorder(on_call=lambda: test_ip2)
         mock_pig.get_current_ip_action = mc
         self.assertEqual(0, mc.count)
 
@@ -196,7 +216,7 @@ class TestDDNSManager(unittest.TestCase):
         )
 
         test_ip2 = 'test_ip2'
-        mc = CallRecorder(test_ip2)
+        mc = CallRecorder(on_call=lambda: test_ip2)
         mock_pig.get_current_ip_action = mc
         self.assertEqual(0, mc.count)
 
@@ -206,7 +226,7 @@ class TestDDNSManager(unittest.TestCase):
 
     def test_ddns_ip_update_should_call_ddns_updater(self):
         # update_ddns should call DDNSUpdater if ddns ip is not outdated
-        mc = CallRecorder(None)
+        mc = CallRecorder(on_call=lambda: None)
         mock_pig = MockPublicIPGetter(get_current_ip_action=lambda: "whatever")
         mock_du = MockDDNSUpdater(update_ddns_record_action=mc)
 
@@ -227,7 +247,7 @@ class TestDDNSManager(unittest.TestCase):
 
     def test_ddns_ip_update_should_not_call_ddns_updater_if_ip_has_not_changed(self):
         # update_ddns should not call DDNSUpdater if ddns ip is not outdated
-        mc = CallRecorder(None)
+        mc = CallRecorder(on_call=lambda: None)
         mock_pig = MockPublicIPGetter(get_current_ip_action=lambda: "whatever")
         mock_du = MockDDNSUpdater(update_ddns_record_action=mc)
 
@@ -246,3 +266,129 @@ class TestDDNSManager(unittest.TestCase):
         self.assertEqual(pium.ddns_ip, pium.current_ip)
         pium.update_ddns(update_current_ip=True)
         self.assertEqual(1, mc.count)
+
+    def test_loop_lifecycle(self):
+        # loop should keep verifying if the the the public ip changed every X seconds and
+        # if changed try to updated it in case of error  the error handler should be called
+        # indefinitely, the loop only stops if the error handler fails
+
+        # Scenario:
+        #
+        # Iteration cycle : 1s
+        #
+        # |         When          |    PublicIpGetter    |          DDNSUpdater           |         ErrorHandler         |
+        # | :-------------------: | :------------------: | :----------------------------: | :--------------------------: |
+        # | Manager instantiation |     () -> 'ip1'      |               -                |              -               |
+        # |     Loop 1st pass     |     () -> 'ip1'      |        ('ip1') -> None         |              -               |
+        # |     Loop 2nd pass     |     () -> 'ip1'      |               -                |              -               |
+        # |     Loop 3rd pass     |     () -> 'ip2'      | ('ip2') -> Raise UpdateDDNSErr |   (UpdateDDNSErr) -> None    |
+        # |     Loop 4th pass     |     () -> 'ip2'      |        ('ip2') -> None         |              -               |
+        # |     Loop 5th pass     |     () -> 'ip2'      |               -                |              -               |
+        # |     Loop 6th pass     | () -> raise GetIpErr |               -                | (GetIpErr) -> raise GetIpErr |
+        #
+        # Loops stops after 6th pass
+
+        def ex(exception):
+            def caller():
+                raise exception
+
+            return caller
+
+        class GetIpErr(Exception):
+            pass
+
+        def get_current_ip_responses():
+            yield lambda: 'ip1'  # instantiation
+            yield lambda: 'ip1'  # 1st pass
+            yield lambda: 'ip1'  # 2nd pass
+            yield lambda: 'ip2'  # 3th pass
+            yield lambda: 'ip2'  # 4th pass
+            yield lambda: 'ip2'  # 5th pass
+            yield ex(GetIpErr)  # 6th pass
+            yield lambda: 'wont hit'  # won't pass
+            yield lambda: 'wont hit'  # won't pass
+
+        g = get_current_ip_responses()
+
+        def get_current_ip_responder():
+            return next(g)()
+
+        get_current_ip_recorder = CallRecorder(on_call=get_current_ip_responder)
+        mock_pig = MockPublicIPGetter(get_current_ip_action=get_current_ip_recorder)
+
+        class UpdateDDNSErr(Exception):
+            pass
+
+        def update_ddns_record_responses():
+            yield lambda: None  # 1st pass (first update)
+            yield ex(UpdateDDNSErr)  # 3st pass
+            yield lambda: None  # 4st pass (because the above pass failed)
+
+        u = update_ddns_record_responses()
+
+        def update_ddns_record_responder():
+            return next(u)()
+
+        update_ddns_record_recorder = CallRecorder(update_ddns_record_responder)
+        mock_du = MockDDNSUpdater(update_ddns_record_action=update_ddns_record_recorder)
+
+        class OnErrErr(Exception):
+            pass
+
+        def on_error_responses():
+            yield lambda: None  # 3st pass
+            yield ex(OnErrErr)  # 6st pass -> Loop stopped
+
+        o = on_error_responses()
+
+        def on_error_responder():
+            return next(o)()
+
+        on_error = CallRecorder(on_error_responder)
+
+        pium = DDNSManager(
+            ip_getter=mock_pig,
+            ddns_updater=mock_du
+        )
+
+        # On instantiation nothing is 'touched'
+        self.assertEqual(1, get_current_ip_recorder.count)
+        self.assertEqual(0, update_ddns_record_recorder.count)
+        self.assertEqual(0, on_error.count)
+
+        interval = 1  # seconds
+
+        # Loop should stop if on_error callable raises error
+        self.assertRaises(OnErrErr, pium.update_loop, interval=interval, on_error=on_error)
+
+        # Ip Getter is called 7 times
+        self.assertEqual(7, get_current_ip_recorder.count)
+
+        # The interval between each update try should be the one explicitly defined in the loop
+        accepted_diff = timedelta(seconds=interval/100)  # no particular reason for choosing 1% of interval as the maximum accepted error
+        expected_interval = timedelta(seconds=interval)
+        for index in range(2, len(get_current_ip_recorder.calls)):  # 1st interval skipped (it' outside the loop)
+            actual_interval = \
+                get_current_ip_recorder.calls[index]['timestamp'] \
+                - get_current_ip_recorder.calls[index - 1]['timestamp']
+            diff = abs(actual_interval - expected_interval)
+            self.assertLess(diff, accepted_diff)
+
+        # DDNS Updater is called 3 times
+        self.assertEqual(3, update_ddns_record_recorder.count)
+        #   1. Once for setting the ip = 'ip1'
+        self.assertEqual(update_ddns_record_recorder.calls[0]['kwargs']['ip'], 'ip1')
+        self.assertIsNone(update_ddns_record_recorder.calls[0]['response'])
+        #   2. The second time when ip = 'ip2', but it fails
+        self.assertEqual(update_ddns_record_recorder.calls[1]['kwargs']['ip'], 'ip2')
+        self.assertIsInstance(update_ddns_record_recorder.calls[1]['exception'], UpdateDDNSErr)
+        #   3. Since it fails it is called again
+        self.assertEqual(update_ddns_record_recorder.calls[2]['kwargs']['ip'], 'ip2')
+        self.assertIsNone(update_ddns_record_recorder.calls[2]['response'])
+
+        # Error handler is called 2 times
+        self.assertEqual(2, on_error.count)
+        #   1. When update_ddns fails
+        self.assertIsInstance(on_error.calls[0]['args'][0], UpdateDDNSErr)
+        #   2. When get_ip fails
+        self.assertIsInstance(on_error.calls[1]['args'][0], GetIpErr)
